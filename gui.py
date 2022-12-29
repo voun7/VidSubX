5add5519424aabe6aadc3614dcbd409d532b87e4
victor n <nwaeze_victor@yahoo.com>
2022-12-29 08:03:41 +0100

    open file dialog implemented

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
 
 class SubtitleExtractorGUI:
     def __init__(self, root):
         self.root = root
         self._create_layout()
+        self.video_paths = None
 
     def _create_layout(self):
         self.root.title("Video Subtitle Extractor")
@@ -38,8 +40,8 @@ class SubtitleExtractorGUI:
         menubar.add_cascade(menu=menu_file, label="File")
         menubar.add_cascade(menu=menu_settings, label="Settings")
 
-        menu_file.add_command(label="Open", command=self._open_file)
-        menu_file.add_command(label="Close", command=self._close_window)
+        menu_file.add_command(label="Open", command=self._open_files)
+        menu_file.add_command(label="Close", command=self._close_main_window)
 
         menu_settings.add_command(label="Language", command=self._language_settings)
         menu_settings.add_command(label="Extraction", command=self._extraction_settings)
@@ -62,13 +64,13 @@ class SubtitleExtractorGUI:
         output_frame = ttk.Frame(self.main_frame)
         output_frame.grid(row=2)
 
-        self.text_output_widget = Text(output_frame, width=97, height=12, state="disabled")
-        self.text_output_widget.grid()
+        self._text_output_widget = Text(output_frame, width=97, height=12, state="disabled")
+        self._text_output_widget.grid()
 
-        output_scroll = ttk.Scrollbar(output_frame, orient=VERTICAL, command=self.text_output_widget.yview)
+        output_scroll = ttk.Scrollbar(output_frame, orient=VERTICAL, command=self._text_output_widget.yview)
         output_scroll.grid(column=1, row=0, sticky="N,S")
 
-        self.text_output_widget.configure(yscrollcommand=output_scroll.set)
+        self._text_output_widget.configure(yscrollcommand=output_scroll.set)
 
         output_frame.grid_columnconfigure(0, weight=1)
         output_frame.grid_rowconfigure(0, weight=1)
@@ -79,10 +81,16 @@ class SubtitleExtractorGUI:
     def _extraction_settings(self):
         pass
 
-    def _open_file(self):
-        pass
+    def _open_files(self):
+        title = "Select Video file"
+        file_types = (("mp4", "*.mp4"), ("mkv", "*.mkv"), ("all files", "*.*"))
+        filenames = filedialog.askopenfilenames(title=title, filetypes=file_types)
+        if filenames:
+            for name in filenames:
+                self.text_to_output(f"Opened file: {name}")
+            self.video_paths = filenames
 
-    def _close_window(self):
+    def _close_main_window(self):
         self._stop_run()
         self.root.quit()
 
@@ -90,18 +98,18 @@ class SubtitleExtractorGUI:
         self.interrupt = True
         self.run_button.configure(text="Run", command=self._run)
 
-    def _text_to_output(self, text):
-        self.text_output_widget.configure(state="normal")
-        self.text_output_widget.insert("end", f"{text}\n")
-        self.text_output_widget.see("end")
-        self.text_output_widget.configure(state="disabled")
+    def text_to_output(self, text):
+        self._text_output_widget.configure(state="normal")
+        self._text_output_widget.insert("end", f"{text}\n")
+        self._text_output_widget.see("end")
+        self._text_output_widget.configure(state="disabled")
 
     def long_running_method(self, count=0):
         num = 1000
         self.progress_bar.configure(maximum=num)
         if self.interrupt:
             return
-        self._text_to_output(f"Line {count} of {num}")
+        self.text_to_output(f"Line {count} of {num}")
         self.progress_bar['value'] += 1
         if count == num:
             self._stop_run()
@@ -112,6 +120,12 @@ class SubtitleExtractorGUI:
         self.interrupt = False
         self.run_button.configure(text='Stop', command=self._stop_run)
         self.progress_bar['value'] = 0
+
+        if self.video_paths:
+            pass
+            # self.text_to_output(self.video_paths)
+        else:
+            self.text_to_output("No video has been selected!")
 
         self.long_running_method()
 
