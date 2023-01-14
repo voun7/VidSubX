a303aa4ddbfac3e74288ae01c23241a351566b34
victor n <nwaeze_victor@yahoo.com>
2023-01-14 16:01:22 -0500

    major update that improves correctness of subtitles

--- main.py
+++ main.py
@@ -64,37 +64,115 @@ class SubtitleExtractor:
             shutil.rmtree(self.vd_output_dir)
             logger.debug("Emptying cache...")
 
-    @staticmethod
-    def similarity(text1: str, text2: str, similarity_threshold: float = 0.8) -> float:
-        return SequenceMatcher(a=text1, b=text2).quick_ratio() > similarity_threshold
-
-    def _remove_duplicate_texts(self) -> None:
-        logger.info("Deleting duplicate texts...")
+    def _remove_duplicate_texts(self, divider: str) -> None:
+        """
+        Remove all texts from text output that don't have the given divider in their name.
+        """
+        logger.debug("Deleting duplicate texts...")
         for file in self.text_output.iterdir():
-            if "--" not in file.name:
+            if divider not in file.name:
                 file.unlink()
 
-    def _merge_similar_texts(self) -> None:
-        no_of_files = len(list(self.text_output.iterdir())) - 1
-        counter = 0
+    def _merge_adjacent_equal_texts(self, divider) -> None:
+        """
+        Merge texts that are beside each other and are the exact same.
+        Use divider for duration in text name.
+        """
+        logger.debug("Merging adjacent equal texts")
+        no_of_files = len(list(self.text_output.iterdir()))
+        counter = 1
         starting_file = None
         for file1, file2 in pairwise(natsorted(self.text_output.iterdir())):
-            similarity = self.similarity(file1.read_text(encoding="utf-8"), file2.read_text(encoding="utf-8"))
+            file1_text = file1.read_text(encoding="utf-8")
+            file2_text = file2.read_text(encoding="utf-8")
             counter += 1
-            if similarity and counter != no_of_files:
-                # print(file1.name, file2.name, similarity)
+            # print(file1.name, file2.name, file1_text, file2_text)
+            if file1_text == file2_text and counter != no_of_files:
                 if not starting_file:
                     starting_file = file1
             else:
-                # print(file1.name, file2.name, similarity)
-                if not starting_file:
+                if not starting_file:  # This condition is used when the file doesn't match the previous or next file.
                     starting_file = file1
                 ending_file = file1
-                if starting_file == ending_file:
+                if counter == no_of_files:
                     ending_file = file2
-                new_file_name = f"{starting_file.stem}--{ending_file.stem}.txt"
+                new_file_name = f"{starting_file.stem}{divider}{ending_file.stem}.txt"
                 starting_file.rename(f"{starting_file.parent}/{new_file_name}")
                 starting_file = None
+
+    @staticmethod
+    def similarity(text1: str, text2: str) -> float:
+        return SequenceMatcher(a=text1, b=text2).quick_ratio()
+
+    @staticmethod
+    def _similar_text_name_gen(start_name: str, end_name: str, divider: str, old_divider) -> str:
+        """
+        Takes 2 file name durations and creates a new file name.
+        """
+        start_name = start_name.split(old_divider)[0]
+        end_name = end_name.split(old_divider)[1]
+        new_name = f"{start_name}{divider}{end_name}.txt"
+        return new_name
+
+    @staticmethod
+    def _name_to_duration(name: str, divider: str) -> float:
+        """
+        Takes a name with two numbers and subtracts to get the duration.
+        :param name: name numbers should seperated by identifier.
+        :param divider: value for splitting string.
+        :return: duration
+        """
+        name_timecode = name.split(divider)
+        duration = float(name_timecode[1]) - float(name_timecode[0])
+        return duration
+
+    def _merge_adjacent_similar_texts(self, old_div, divider, similarity_threshold: float = 0.65) -> None:
+        """
+        Merge texts that are not the same but beside each other and similar.
+        The text that has the longest duration becomes the text for all similar texts.
+        :param similarity_threshold: cut off point to determine similarity.
+        """
+        logger.debug("Merging adjacent similar texts")
+        no_of_files = len(list(self.text_output.iterdir()))
+        counter = 1
+        starting_file = file_text = file_duration = None
+        for file1, file2 in pairwise(natsorted(self.text_output.iterdir())):
+            file1_text, file1_duration = file1.read_text(encoding="utf-8"), self._name_to_duration(file1.stem, old_div)
+            file2_text, file2_duration = file2.read_text(encoding="utf-8"), self._name_to_duration(file2.stem, old_div)
+            similarity = self.similarity(file1_text, file2_text)
+            counter += 1
+            # print(f"File 1 Name: {file1.name}, Duration: {file1_duration}, Text: {file1_text}\n"
+            #       f"File 2 Name: {file2.name}, Duration: {file2_duration}, Text: {file2_text}\n"
+            #       f"File 1 & 2 Similarity: {similarity}\n")
+            if similarity >= similarity_threshold and counter != no_of_files:
+                if not starting_file:
+                    starting_file = file1
+                    file_text = file1_text
+                    file_duration = file1_duration
+
+                if file2_duration > file_duration:  # Change text and duration when longer duration is found.
+                    file_text = file2_text
+                    file_duration = file2_duration
+            else:
+                if not starting_file:  # This condition is used when the file doesn't match the previous or next file.
+                    starting_file = file1
+                    file_text = file1_text
+
+                ending_file = file1
+
+                new_name = self._similar_text_name_gen(starting_file.stem, ending_file.stem, divider, old_div)
+                new_file_name = f"{self.text_output}/{new_name}"
+                with open(new_file_name, 'w', encoding="utf-8") as text_file:
+                    text_file.write(file_text)
+
+                if counter == no_of_files:
+                    new_name = file2.name.replace(old_div, divider)
+                    new_file_name = f"{self.text_output}/{new_name}"
+                    file_text = file2_text
+                    with open(new_file_name, 'w', encoding="utf-8") as text_file:
+                        text_file.write(file_text)
+
+                starting_file = file_text = file_duration = None
 
     @staticmethod
     def timecode(frame_no: float) -> str:
@@ -116,9 +194,9 @@ class SubtitleExtractor:
         if name.exists():
             current_time = time.strftime("%H;%M;%S")
             name = f"{name.parent}/{name.stem} {current_time} (new copy).srt"
-        logger.info(f"Subtitle file successfully generated. Name: {name}")
         with open(name, 'w', encoding="utf-8") as new_sub:
             new_sub.writelines(lines)
+        logger.info(f"Subtitle file generated. Name: {name}")
 
     def _generate_subtitle(self) -> None:
         """
@@ -131,12 +209,14 @@ class SubtitleExtractor:
             return
 
         logger.info("Generating subtitle...")
-        self._merge_similar_texts()
-        self._remove_duplicate_texts()
+        self._merge_adjacent_equal_texts("--")
+        self._remove_duplicate_texts("--")
+        self._merge_adjacent_similar_texts("--", "---")
+        self._remove_duplicate_texts("---")
         subtitles = []
         line_code = 0
         for file in natsorted(self.text_output.iterdir()):
-            file_name = file.stem.split("--")
+            file_name = file.stem.split("---")
             line_code += 1
             frame_start = self.timecode(float(file_name[0]))
             frame_end = self.timecode(float(file_name[1]))
