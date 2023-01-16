c2595acb461749d699ffe7f14ce4862909926763
victor n <nwaeze_victor@yahoo.com>
2023-01-16 10:11:17 -0500

    doc string and type hint added

--- utilities/utils.py
+++ utilities/utils.py
@@ -7,12 +7,18 @@ class Process:
     interrupt_process = False
 
     @classmethod
-    def start_process(cls):
+    def start_process(cls) -> None:
+        """
+        Allows process to run.
+        """
         cls.interrupt_process = False
         logger.debug(f"interrupt_process set to: {cls.interrupt_process}")
 
     @classmethod
-    def stop_process(cls):
+    def stop_process(cls) -> None:
+        """
+        Stops process from running.
+        """
         cls.interrupt_process = True
         logger.debug(f"interrupt_process set to: {cls.interrupt_process}")
 
