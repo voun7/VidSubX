e24961147f21367c0e01b8cd1b91b16a9c2f7c35
victor n <nwaeze_victor@yahoo.com>
2023-01-18 06:51:24 -0500

    comments changed

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ class SubtitleExtractor:
                     file_duration = file1_duration
 
                 if file2_duration > file_duration:  # Change text and duration when longer duration is found.
-                    # print(f"--------longer duration found: {file2_duration}")
+                    # print(f"--- Longer duration found: {file2_duration} ---")
                     file_text = file2_text
                     file_duration = file2_duration
             else:
@@ -164,8 +164,7 @@ class SubtitleExtractor:
 
                 new_name = self._similar_text_name_gen(starting_file.stem, ending_file.stem, divider, old_div)
                 new_file_name = f"{self.text_output}/{new_name}"
-                # print(f"New file name: {new_file_name}\n"
-                #       f"New file text: {file_text}\n")
+                # print(f"New file name: {new_file_name}\n New file text: {file_text}\n")
                 with open(new_file_name, 'w', encoding="utf-8") as text_file:
                     text_file.write(file_text)
 
@@ -173,8 +172,7 @@ class SubtitleExtractor:
                     new_name = file2.name.replace(old_div, divider)
                     new_file_name = f"{self.text_output}/{new_name}"
                     file_text = file2_text
-                    # print(f"New file name: {new_file_name}\n"
-                    #       f"New file text: {file_text}\n")
+                    # print(f"New file name: {new_file_name}\n New file text: {file_text}\n")
                     with open(new_file_name, 'w', encoding="utf-8") as text_file:
                         text_file.write(file_text)
 
