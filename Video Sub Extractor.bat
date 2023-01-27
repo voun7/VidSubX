e54cc3bba7a5ce7941e6a5318c83ccfdf52f260b
victor n <nwaeze_victor@yahoo.com>
2023-01-27 14:14:48 -0500

    modified to fit system

--- Video Sub Extractor.bat
+++ Video Sub Extractor.bat
@@ -6,9 +6,9 @@ rem - conda to be in the PATH
 rem - cmd.exe to be initialized with conda init
 
 rem Define here the path to your conda installation
-set CONDAPATH=C:\ProgramData\Miniconda3
+set CONDAPATH=C:\Users\VOUN-XPS\miniconda3
 rem Define here the name of the environment
-set ENVNAME=someenv
+set ENVNAME=VSE
 
 rem The following command activates the base environment.
 rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
@@ -19,7 +19,7 @@ rem Using call is required here, see: https://stackoverflow.com/questions/246781
 call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
 
 rem Run a python script in that environment
-python script.py
+python gui.py
 
 rem Deactivate the environment
 call conda deactivate
