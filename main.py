6b858735ff8e82031981bf8b7b10486d85527083
victor n <nwaeze_victor@yahoo.com>
2023-03-05 13:32:19 -0500

    values replaced by utils variables

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ class SubtitleDetector:
         # Extracted video frame storage directory
         self.frame_output = self.vd_output_dir / "sub detect frames"
 
-    def get_key_frames(self, split_start: int = 4, split_stop: int = 2, no_of_frames: int = 400) -> None:
+    def get_key_frames(self, split_start: int = utils.Config.split_start, split_stop: int = utils.Config.split_stop,
+                       no_of_frames: int = utils.Config.no_of_frames) -> None:
         """
         Extract specific parts of video that may contain subtitles.
         :param split_start: value used to divide total frame to choose start point
@@ -72,7 +73,8 @@ class SubtitleDetector:
         for frames in frame_chunks:
             extract_frames(self.video_file, self.frame_output, key_area, frames[0], frames[1], self.fps)
 
-    def _pad_sub_area(self, top_left: tuple, bottom_right: tuple, x_padding: int = 150, y_padding: int = 10) -> tuple:
+    def _pad_sub_area(self, top_left: tuple, bottom_right: tuple, x_padding: int = utils.Config.x_padding,
+                      y_padding: int = utils.Config.y_padding) -> tuple:
         """
         Prevent boundary box from being too close to text by adding padding.
         """
