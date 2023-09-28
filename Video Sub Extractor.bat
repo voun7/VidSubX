49d3764d33b7ede08c2bfb1e4cb5c1c5966b5419
Victor N <nwaeze_victor@yahoo.com>
2023-09-27 22:19:37 -0400

    variable used for username

--- Video Sub Extractor.bat
+++ Video Sub Extractor.bat
@@ -6,7 +6,7 @@ rem - conda to be in the PATH
 rem - cmd.exe to be initialized with conda init
 
 rem Define here the path to your conda installation
-set CONDAPATH=C:\Users\nwaez\miniconda3
+set CONDAPATH=C:\Users\%USERNAME%\miniconda3
 rem Define here the name of the environment
 set ENVNAME=VSE
 
